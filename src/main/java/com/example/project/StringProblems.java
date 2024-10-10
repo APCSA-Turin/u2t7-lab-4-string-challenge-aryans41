5e2c13beb765bb0a0b91b5eedfc9b247b0be5eab
aryans41 <aryans41@nycstudents.net>
2024-10-10 16:32:41 +0000

    second comm

--- src/main/java/com/example/project/StringProblems.java
+++ src/main/java/com/example/project/StringProblems.java
@@ -9,7 +9,11 @@ public class StringProblems{
     // endsLy("y") → false
     // endsLy("oddy") → false
     public boolean endsLy(String x){
-            //implement code here
+        if (x.length() > 1) {
+            if (x.substring(x.length() - 2).equals("ly")) {
+                return true;
+            }
+        } 
         return false;
     }
 
@@ -22,7 +26,12 @@ public class StringProblems{
     // conCat("abc", "") → "abc"
     public String conCat(String s1, String s2){
         //implement code here
-        return "";
+        if(!(s2.equals(""))){
+            if((s1.substring(s1.length()-1)).equals(s2.substring(0, 1))){
+            return s1+s2.substring(1);
+            }
+        }
+        return s1+s2;
     }
 
     // Given a string, return a version without the first 2 chars. 
@@ -35,10 +44,19 @@ public class StringProblems{
     // deFront("abeep") -> "abeep"
     public String deFront(String s1){
         //implement code here
-        return "";
+        if((s1.substring(0,1)).equals("a")){
+            if((s1.substring(1,2)).equals("b")){
+                return s1;
+            }
+            return "a" + s1.substring(2);
+        }
+        if((s1.substring(1,2)).equals("b")){
+            return s1.substring(1);
+        }
+            return s1.substring(2);
     }
 
-    
+
     // Given a string, if the first or last chars are 'x', 
     // return the string without those 'x' chars, and otherwise 
     // return the string unchanged.
@@ -46,9 +64,18 @@ public class StringProblems{
     // withoutX("xHi") → "Hi"
     // withoutX("Hxix") → "Hxi"
     public String withoutX(String s1){
-        return "";
-    }
-
+        if (s1.substring(0,1).equals("x") && s1.substring(s1.length() - 1, s1.length()).equals("x")) {
+            return s1.substring(1, s1.length() - 1); 
+        } 
+        if (s1.substring(0,1).equals("x")) {
+            return s1.substring(1, s1.length()); 
+        } 
+        if (s1.substring(s1.length() - 1, s1.length()).equals("x")) {
+            return s1.substring(0, s1.length() - 1); 
+        }
+            return s1;  
+    } 
+         
     // Given a string str, if the string starts with "f" return "Fizz".
     //  If the string ends with "b" return "Buzz". If both the "f" and "b" 
     //  conditions are true, return "FizzBuzz". In all other cases, return the 
@@ -57,9 +84,18 @@ public class StringProblems{
     // fizzString("dib") → "Buzz"
     // fizzString("fib") → "FizzBuzz"
     public String fizzString(String s1){
-        return "";
+        if((s1.substring(0,1).equals("f"))&&(s1.substring(s1.length()-1)).equals("b")){
+            return "FizzBuzz";
+        }
+        if(s1.substring(0,1).equals("f")){
+            return "Fizz";
+        }
+        if((s1.substring(s1.length()-1)).equals("b")){
+            return "Buzz";
+        }
+        return s1;
     }
-
+    
     // Given an int n, return the string form of the number followed 
     // by "!". So the int 6 yields "6!". Except if the number is 
     // divisible by 3 use "Fizz" instead of the number, and if the 
@@ -70,6 +106,17 @@ public class StringProblems{
     // fizzString2(2) → "2!"
     // fizzString2(3) → "Fizz!"
     public String fizzString2(int x){
-        return "";
-    }
-}
+        if ((x % 3 == 0) && (x % 5 == 0)) {
+            return "FizzBuzz!";
+        }
+        if (x % 3 == 0) {
+            return "Fizz!";
+        }
+        if (x % 5 == 0) {
+            return "Buzz!"; 
+        }
+        return x + "!"; 
+        }
+        }
+        
+    
